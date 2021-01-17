8f8f5e6e09011c323fb291c3fea494b53e35fe90
AKSHAY KR <ashayakr4@gmail.com>
2021-01-17 11:52:11 +0530

    workshop

---
+++ pythoncollections/listprogram/qstn_2.py
@@ -0,0 +1,10 @@
+lst=[1,2,3,4,5,6]
+num=int(input("Enter the sum that u want : "))
+for i in lst:
+    for j in lst:
+        if i+j==num:
+            print(i,",",j," ")
+        else:
+            pass
+
+
---
+++ pythoncollections/listprogram/questn_1.py
@@ -0,0 +1,10 @@
+lst=[2,6,4]
+c=0
+nxt=0
+for num in lst:
+    c=sum(lst)
+    nxt=c-num
+    print(nxt)
+
+
+    
