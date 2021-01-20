84f197235a163c02469dad9e8f753daee8ee5d5a
AKSHAY KR <ashayakr4@gmail.com>
2021-01-20 16:12:26 +0530

    updation

--- pythoncollections/dictionary/wordcount.py
+++ pythoncollections/dictionary/wordcount.py
@@ -1,5 +1,11 @@
-word="hello hai hello hai hello"
+word="hello welcome to ooty nice to meet you"
 #word count
 #hello:3, hai:2
 #movie
 #covid
+print(word)
+count=0
+for i in word:
+    if i==" ":
+        count+=1
+print("Number of words in the sentence is ",count)
